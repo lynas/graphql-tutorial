0b2cc398a5c869c6387ec9f7dd4c86f7b0c2cf82
Sazzad <sazzad@hsenidmobile.com>
2020-05-13 20:40:09 +0600

    Init

---
+++ src/main/java/com/lynas/graphqltutorial/GraphqlTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.lynas.graphqltutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lynas/graphqltutorial/GraphqlTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lynas.graphqltutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
